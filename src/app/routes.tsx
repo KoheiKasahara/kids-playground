a2ab9deba7a31dd532c305bf620ec5993964c53b
笠原康平 <29470676+KoheiKasahara@users.noreply.github.com>
2026-09-08 10:03:32 +0900

    feat: add animal bath care minigame for preschoolers

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -51,6 +51,7 @@ const MATH_QUIZ_MODES: MathQuizMode[] = ['add', 'sub', 'mul', 'div']
 
 export const routes: RouteObject[] = [
   { path: '/', element: <Home /> },
+  { path: '/games/animal-bath', element: lazyRoute(() => import('../games/animal-bath/AnimalBathPlay')) },
   { path: '/games/bento-builder', element: playRoute(lazyRoute(() => import('../games/bento-builder/BentoBuilderPlay'))) },
   { path: '/games/circuit-racing', element: lazyRoute(() => import('../games/circuit-racing/CircuitRacingPlay')) },
   { path: '/games/oekaki-korokoro', element: playRoute(lazyRoute(() => import('../games/oekaki-korokoro/OekakiKorokoroPlay'))) },
